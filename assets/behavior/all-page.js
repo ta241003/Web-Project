bf8a5e238580ecb36fff4d218dad89c334bad49a
NguyenAnh2003 <cunho2803032003@gmail.com>
2022-04-17 10:53:48 +0700

    new updated

--- assets/behavior/all-page.js
+++ assets/behavior/all-page.js
@@ -2,5 +2,5 @@
 var login_btn = document.getElementById('login-btn');
 var showForm = document.getElementById('form-account')
 login_btn.addEventListener('click', function () {
-    showForm.classList.toggle('.show')
+    showForm.classList.toggle('show')
 });
