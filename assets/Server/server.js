6c163654e031b3776af11f1132afed3da6b7534a
NguyenAnh2003 <cunho2803032003@gmail.com>
2022-04-18 12:21:20 +0700

    new update

--- assets/Server/server.js
+++ assets/Server/server.js
@@ -2,11 +2,14 @@ const express = require('express');
 require('dotenv').config(); // include env file
 const app = express();
 
-app.use("/Client", express.static('/Client')); // use static file
+app.use(express.static('Client')); // use static file
 
 
 app.get('/', (req, res) => {
     res.sendFile('/Home-page.html');
-})
+});
 
 app.listen(process.env.PORT, ()=>console.log("Running . . ."));
+
+
+
