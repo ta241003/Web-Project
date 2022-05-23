75484485ac39ac745fa6a62b90b20de0ce37b44a
NguyenAnh2003 <cunho2803032003@gmail.com>
2022-05-23 21:07:58 +0700

    redecorate room item

--- assets/server.js
+++ assets/server.js
@@ -5,19 +5,19 @@ const encoder = bodyParser.urlencoded();
 require('dotenv').config(); // include env file
 const app = express();
 
-app.use("/src", express.static('src')); // use static file
 
-// app.post('/')
-
-
+app.use(express.json());
 app.use(cors());
+app.use("/assets/src", express.static('src')); // use static file
+app.post('/contact', encoder, (req, res) => {
+    var email = req.body.email;
+    res.redirect('/');
+});
+
+
 // get home page
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/test.html');
 });
-
-
-
-
 
 app.listen(process.env.PORT, ()=>console.log("Running . . ."));
